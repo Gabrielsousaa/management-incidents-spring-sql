d2ff138430e0f0916d93a0ba214a4243629c7ea6
Gabriel de Sousa Souto <gsousax2008@Hotmail.com>
2021-07-26 23:13:05 -0300

    Testando o REST...

--- src/main/java/com/gabrielsousa/security/resources/IncidentResource.java
+++ src/main/java/com/gabrielsousa/security/resources/IncidentResource.java
@@ -1,8 +1,17 @@
 package com.gabrielsousa.security.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/incidents")
 public class IncidentResource{
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public String List(){
+        return "Rest is working..";
+    }   
+
 
 }
